824f00ccddd369d1425553f3953d0d975244e9ee
Hyp3ri0n <aurelien2405@gmail.com>
2018-02-13 08:55:01 +0100

    FIX alarme route

--- src/app/components/root/header/header.component.ts
+++ src/app/components/root/header/header.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Alarme, AlarmeService } from '../../../services/alarmes/alarme.service';
+import { HttpService } from '../../../services/http/http.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './app/components/root/header/header.view.html',
 })
 
-export class HeaderComponent {
-    show : string;
+export class HeaderComponent implements OnDestroy {
+    private show : string = 'none';
 
-    constructor() { this.show = 'none'; }
+    private alarmes : Alarme[] = [];
+
+    constructor(private alarme : AlarmeService, private http : HttpService) {
+        this.getData();
+        this.http.backOnlineEventListener = { component : 'HeaderComponent', cb : () => this.getData() };
+    }
+
+    public ngOnDestroy(): void {
+        this.http.removeBackOnlineListener('HeaderComponent');
+    }
+
+    private getData() : void {
+        this.alarme.getLastAlarmes().subscribe(
+            alarmes => {
+                this.alarmes = alarmes;
+            }
+        );
+    }
 
     private go_clicked() : void {
         if (this.show === 'none') {
